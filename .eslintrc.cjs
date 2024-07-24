bd753d2275d0b9885458b6a7cf143d89e4ee70ac
Lea Muñoz <munozleandrom96@gmail.com>
2024-07-24 15:58:55 -0300

    Iniciado calendar app

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/recommended',
+		'plugin:eslint-config-prettier',
+		'prittier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		'no-debugger': 'warn',
+	},
+};vvv
